3caf40e4228ff1c9e40b0439e6ca044982513a97
Taichi Uragami <backpaper0@gmail.com>
2015-07-15 21:02:08 +0900

    PathParamを利用してファイル名を限定しない感じで

--- src/main/java/sample/StaticResources.java
+++ src/main/java/sample/StaticResources.java
@@ -1,31 +1,44 @@
 package sample;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("static")
 public class StaticResources {
+    private static final ConcurrentMap<String, MediaType> types;
 
-    @Path("hello.txt")
+    @Path("{name}")
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public InputStream getHello() throws IOException {
+    public Response getResource(@PathParam("name") String name)
+            throws IOException {
 
-        URL resource = getClass().getResource("/static/hello.txt");
+        URL resource = getClass().getResource("/static/" + name);
         if (resource == null) {
             throw new NotFoundException();
         }
 
         URLConnection con = resource.openConnection();
 
-        return con.getInputStream();
+        String ext = name.substring(name.lastIndexOf('.'));
+
+        MediaType type = types.get(ext);
+
+        return Response.ok(con.getInputStream(), type).build();
+    }
+
+    static {
+        types = new ConcurrentHashMap<>();
+        types.put(".txt", MediaType.TEXT_PLAIN_TYPE);
+        types.put(".js", MediaType.valueOf("text/javascript"));
     }
 }
--- src/test/java/sample/StaticResourcesTest.java
+++ src/test/java/sample/StaticResourcesTest.java
@@ -3,6 +3,8 @@ package sample;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -27,6 +29,34 @@ public class StaticResourcesTest extends JerseyTest {
                 is(MediaType.TEXT_PLAIN_TYPE));
         assertThat("Entity body", response.readEntity(String.class),
                 is("Hello, world!"));
+    }
+
+    /**
+     * JavaScriptファイルも返される事を確認する。
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testGetFibJs() throws Exception {
+        Response response = target("static/fib.js").request().get();
+        assertThat("Status code", response.getStatusInfo(), is(Status.OK));
+        assertThat("Conetnt-Type", response.getMediaType(),
+                is(MediaType.valueOf("text/javascript")));
+
+        ScriptEngine engine = new ScriptEngineManager()
+                .getEngineByMimeType("text/javascript");
+        engine.eval(response.readEntity(String.class));
+
+        assertThat("fib(0)", engine.eval("fib(0)"), is(0));
+        assertThat("fib(1)", engine.eval("fib(1)"), is(1));
+        assertThat("fib(2)", engine.eval("fib(2)"), is(1.0));
+        assertThat("fib(3)", engine.eval("fib(3)"), is(2.0));
+        assertThat("fib(4)", engine.eval("fib(4)"), is(3.0));
+        assertThat("fib(5)", engine.eval("fib(5)"), is(5.0));
+        assertThat("fib(6)", engine.eval("fib(6)"), is(8.0));
+        assertThat("fib(7)", engine.eval("fib(7)"), is(13.0));
+        assertThat("fib(8)", engine.eval("fib(8)"), is(21.0));
+        assertThat("fib(9)", engine.eval("fib(9)"), is(34.0));
     }
 
     @Override
